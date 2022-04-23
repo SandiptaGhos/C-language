8d931b8d53fa9f69f0fe62b86e8ea1e8305915a7
SandiptaGhos <102342894+SandiptaGhos@users.noreply.github.com>
2022-04-23 18:17:10 +0530

    Add files via upload

---
+++ name_DOB_mbno..cpp
@@ -0,0 +1,10 @@
+//write a program to print your name,date of birth and mobile number
+
+#include<stdio.h>
+int main()
+{
+	printf("Name:Sandipta Ghosh\n");
+	printf("Date of birth:28th August 2002\n");
+	printf("Mobile Number:8961829620\n");
+	return 0;
+}
