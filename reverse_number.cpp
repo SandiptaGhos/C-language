a72c877fe49cb3d70e56369584da858877392409
SandiptaGhos <102342894+SandiptaGhos@users.noreply.github.com>
2022-04-23 09:55:23 +0530

    Add files via upload

---
+++ reverse_number.cpp
@@ -0,0 +1,18 @@
+//write a c program to reverse and print a given number
+
+#include<stdio.h>
+int main()
+{
+	int a,rev=0,n;
+	printf("Enter an integer:");
+	scanf("%d",&a);
+	while(a!=0)
+	{
+		n=a%10;
+		rev=rev*10+n;
+		a/=10;
+	}
+	printf("Reversed number=%d",n);
+	return 0;
+}
+
