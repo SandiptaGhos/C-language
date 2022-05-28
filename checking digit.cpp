061d1eca7f5e184b19427dbd6748068f00893c8b
Sandipta Ghosh <102342894+SandiptaGhos@users.noreply.github.com>
2022-05-28 11:42:46 +0530

    Add files via upload

---
+++ checking digit.cpp
@@ -0,0 +1,18 @@
+//write a program in c to check whether a character is digit or not
+
+#include<stdio.h>
+int main()
+{
+  char ch;
+  printf("Enter chracter: ");
+  scanf("%c", &ch);
+  if(ch>='0' && ch<='9')
+  {
+	printf("%c is DIGIT.", ch);
+  }
+  else
+  {
+   printf("%c is NOT DIGIT.", ch);
+  }
+  return 0;
+}
